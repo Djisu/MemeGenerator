dda0faa7922e4ddcba17b8fa84286178b3457a31
Paul Fleischer-Djoleto <pfleischer2002@yahoo.co.uk>
2020-01-31 01:18:48 +0000

    Add files via upload

---
+++ src/App.js
@@ -0,0 +1,19 @@
+import React from 'react';
+import Header from './Header'
+import MemeGenerator from './MemeGenerator'
+import './App.css';
+
+
+
+const App = () => {
+  return (
+    <div>
+      <Header />
+      <MemeGenerator />
+    </div>
+  )
+}
+
+
+
+export default App;
